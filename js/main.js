48f539446e8cd5f92d2d457ae02066b610c6290a
Anna Toll <anna.toll@protonmail.com>
2021-11-25 14:12:00 +0100

    Basic CSS applied

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 let allArtworksList = document.getElementById("filter-art-works"),
     searchInput = document.getElementById('search-input'),
     searchBtn = document.getElementById('search-btn');
-    searchMessage = document.getElementById('search-result-message'),
+    searchMessage = document.getElementById('search-result-messages'),
     searchResultContainer = document.getElementById("search-result-container");
 
 searchInput.addEventListener('keyup', (e) => {
     if (e.target.value.length < 3) {
+        searchResultContainer.innerHTML = '';
         searchMessage.innerText = 'Ange minst 3 tecken i sökfältet.';
         return;
     } 
-    searchMessage.innerText = 'Sökresultat.';
     searchBtn.click();
 })
 
 searchBtn.addEventListener('click', async () => {
     if (searchInput.value == '') return;
     searchResultContainer.innerHTML = '';
+    searchMessage.innerText = 'Söker.';
     let pagesAndResultsArr = apiFetchResultSetup();
     for (let i = 1; i < pagesAndResultsArr[0]; i++) {
         let response = await fetch(`https://api.artic.edu/api/v1/artworks?limit=${pagesAndResultsArr[1]}&page=${i}&fields=id%2Ctitle%2Cartist_title`);
@@ -27,12 +28,10 @@ searchBtn.addEventListener('click', async () => {
                     || artWork.artist_title.toLowerCase().includes(searchInput.value.toLowerCase())
                 ){
                     let matchedArtwork = await fetchArtwork(artWork.id);
-                    console.log(searchResultContainer);
-                    // searchResultContainer.innerHTML = 'Hej hej';
                     searchResultContainer.innerHTML += `
                         <div>
-                            <h3>${matchedArtwork.title}</h3>
-                            <p><strong>${matchedArtwork.artist_title}</strong> ${matchedArtwork.place_of_origin}, ${matchedArtwork.date_display}</p>
+                            <h4>${matchedArtwork.title}</h4>
+                            <p><span class="artist">${matchedArtwork.artist_title}</span><i> ${matchedArtwork.place_of_origin}, ${matchedArtwork.date_display}</i></p>
                             <img src="https://www.artic.edu/iiif/2/${matchedArtwork.image_id}/full/843,/0/default.jpg" class="search-result-img">
                         </div>
                     `;
@@ -46,8 +45,9 @@ searchBtn.addEventListener('click', async () => {
     }
     if (searchResultContainer.innerHTML == '') {
         searchMessage.innerText = 'Sökningen gav inga resultat.';
+    } else {
+        searchMessage.innerText = ' Sökning klar.';
     }
-    searchMessage.innerText += ' Sökning klar.';
 })
 
 function apiFetchResultSetup() {
